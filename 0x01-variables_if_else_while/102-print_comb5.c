626f86da2bb20d1fa8efbe15f40d9b760241b961
Sheddymanna <akinrinade.saheed@yahoo.com>
2023-02-18 11:09:44 -0800

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
 /**
- * main - program that prints all single digit numbers of base 10 from 0
+ * main - program that prints all possible combinations of two two-digit num
  *
- * Return: retun 0.
+ * Return: always return 0 in this case.
  */
 int main(void)
 {
-	int num;
+	int num0, num1;
 
-	for (num = 0; num < 10; num++)
-		printf("%d", num);
-	printf("\n");
-	return (0);
+	for (num0 = 0; num0 <= 98; num0++)
+	{
+		for (num1 = num0 + 1 ; num1 <= 99; num1++)
+		{
+			putchar((num0 / 10) + '0');
+			putchar((num0 % 10) + '0');
+
+			putchar(' ');
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+		if (num0 == 98 && num1 == 99)
+			continue;
+		putchar(',');
+		putchar(' ');
+
+		}
+	}	putchar('\n');
+
+		return (0);
 }
