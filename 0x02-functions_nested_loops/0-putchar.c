0d7dc614764364cec93e33aa6826d986bb464b91
Sheddymanna <akinrinade.saheed@yahoo.com>
2023-02-21 10:50:55 -0800

    There is no such thing as absolute value in this world. You can only estimate what a thing is worth to you

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,4 @@
 #include "main.h"
-/**
- * main - main function
- * _putchar: print the alphabets putchar
- * Return: always 0
- */
 
 int main(void)
 {
@@ -12,9 +7,5 @@ int main(void)
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+	
 }
