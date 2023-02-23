477e5d7445dd8ffbbf2cdd9891c1c9f0171d97b8
Sheddymanna <akinrinade.saheed@yahoo.com>
2023-02-23 03:07:32 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
 
 int main(void)
 {
@@ -7,5 +12,10 @@ int main(void)
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
-	
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
